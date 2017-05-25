ee168fa80994dfa73e44065a83c1fdf5f249870a
Kelly Clemons <kelly.clemons@mac.com>
2017-05-25 15:38:17 -0500

    completed basic html and css for favorites route

--- app/routes/favorites.vue
+++ app/routes/favorites.vue
@@ -1,6 +1,22 @@
 <template lang="html">
   <div class="">
 
+    <section class="main favorites">
+      <div class="post-container favorites-container">
+
+        <div class="heading">
+          <p class="heading-title">My Favorites:</p>
+        </div>
+
+        <!-- NTS: Use CRUD to add/remove favorites -->
+
+        <div class="favorited">
+          <div class="favorited__item"></div>
+        </div>
+
+      </div>
+    </section>
+
   </div>
 </template>
 
